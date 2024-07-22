741b7d7e13bb3a6e6550d44ed3610039842c6694
Paola Silva <paolavic014@outlook.com>
2024-07-22 18:02:50 -0300

    Segunda Parte

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,81 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O nome é obrigatório!")
+	private String nome;
+
+	@NotBlank(message = "O descricao é obrigatório!")
+	private String descricao;
+	
+	@NotBlank(message = "O principio ativo é obrigatório!")
+	private String principio;
+	
+	@NotBlank(message = "A marca é obrigatório!")
+	private String marca;
+
+	@NotNull(message = "O preço é obrigatório!")
+	private float preco;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getPrincipio() {
+		return principio;
+	}
+
+	public void setPrincipio(String principio) {
+		this.principio = principio;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}	
+}
